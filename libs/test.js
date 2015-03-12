e70435dc530f2a7406b459c7b5856d5ad75a9bbb
Jozsef Suba <Jozsef.Suba@doclerholding.com>
2015-03-12 12:33:59 +0100

    merging gabeekex-master into master

--- libs/test.js
+++ libs/test.js
@@ -1,8 +1,20 @@
-module.exports = {
-	a: 0,
-	test: function() {
-		setTimeout(function() {
-			this.a = 1;
-		}.bind(this), 1000);
-	}
+/**
+ * TestClass.
+ *
+ * @constructor
+ */
+var TestClass = function() {
+	this.a = 0;
 };
+
+/**
+ * Check is this a 1000ms timeout.
+ *
+ */
+TestClass.prototype.test = function() {
+	setTimeout(function() {
+		this.a = 1;
+	}.bind(this), 1000);
+};
+
+module.exports = TestClass;
--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,18 @@
 var assert = require('chai').assert,
 	sinon = require('sinon'),
-	testLib = require('../libs/test');
+	TestClass = require('../libs/test');
 
 suite('test project', function() {
 	setup(function() {
 		this.sandbox = sinon.sandbox.create();
 		this.clock = this.sandbox.useFakeTimers();
+		this.testClass = new TestClass();
 	});
 
 	test('test runs', function() {
-		testLib.test();
+		this.testClass.test();
 		this.clock.tick(1000);
-		assert.strictEqual(testLib.a, 1);
+		assert.strictEqual(this.testClass.a, 1);
 	});
 
 	teardown(function() {
